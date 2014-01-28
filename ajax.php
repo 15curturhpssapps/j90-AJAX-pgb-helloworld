4c0f8a57a8af00a6e3a23976aa6470d3e2d1d79a
hpssjellis <jellis@hpssapps.com>
2014-01-28 13:12:56 -0800

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,8 +1,5 @@
 <?php
 
- echo "<table border=1>
-       <tr><td>".rand(1,99)."
-       </td><td>".rand{91,99)."
-       </td></tr></table>";
-echo "you typed".$_POST("myWord");
+  echo "Here is a php server generated random number = ".rand(91,99)."<br>";
+   
 ?>
